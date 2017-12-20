e93669e1dd7092c3fdffd6b33bf8fd08c54d046c
Karl Jensen <karljensen@gmail.com>
2017-12-19 18:41:37 -0800

    Consistent, see.

--- migrations/20170131154454_add_user_id_to_urls.js
+++ migrations/20170131154454_add_user_id_to_urls.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.table("urls", (table) => {
     table.integer("user_id").unsigned();
-    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+    table.foreign("user_id").references("users.id").onDelete("CASCADE");
   });
 };
 exports.down = function(knex) {
